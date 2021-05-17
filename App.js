1353d3a2d8e1dfd125d871d6f3e154ff1f4c9d10
Shatilla Prayer <shatillamac@Shatillas-MacBook-Air.local>
2021-05-17 16:08:30 -0700

    working navigation

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+//import { NavigationContainer } from '@react-navigation/native';
+//import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import detailPage from './components/detailPage';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function MyStack() {
   return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Detail" component={detailPage} />
+    </Stack.Navigator>
+  );
+}
+
+function HomeScreen({navigation}){
+  return(
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Go to Detail" onPress={()=> navigation.navigate('Detail')}></Button>
     </View>
   );
 }
+
+function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +46,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+//const AppContainer = createAppContainer(AppNavigator);
+
+export default App;
---
+++ components/detailPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, Image, ScrollView, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const detailPage = () => {
+  return (
+    <ScrollView>
+      <Text>This is the Detail Page</Text>
+      <View>
+        <Text>Detail Page</Text>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue="You can type in me"
+      />
+    </ScrollView>
+  );
+}
+
+export default detailPage;
